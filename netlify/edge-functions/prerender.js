1c5cc86ac52970fe8dfd132afa2c14648f75161f
mashxura04 <mashxurak64@gmail.com>
2026-08-07 17:27:42 +0500

    Fix edge function: exclude static files from prerender interception

--- netlify/edge-functions/prerender.js
+++ netlify/edge-functions/prerender.js
@@ -1,13 +1,15 @@
 const BOT_PATTERN = /googlebot|bingbot|yandex|baiduspider|facebookexternalhit|twitterbot|linkedinbot|telegrambot|whatsapp|slackbot|discordbot|applebot|duckduckbot/i;
 
+const STATIC_FILE_PATTERN = /\.(xml|txt|json|ico|png|jpg|jpeg|svg|css|js|woff|woff2|ttf)$/i;
+
 export default async (request, context) => {
   const userAgent = request.headers.get("user-agent") || "";
+  const url = new URL(request.url);
 
-  if (!BOT_PATTERN.test(userAgent)) {
+  if (!BOT_PATTERN.test(userAgent) || STATIC_FILE_PATTERN.test(url.pathname)) {
     return context.next();
   }
 
-  const url = new URL(request.url);
   const prerenderUrl = `https://service.prerender.io${url.pathname}${url.search}`;
 
   const prerenderResponse = await fetch(prerenderUrl, {
